0c767a8bd0858b07a8f5b628fa43747294818946
Stefan Mitkovski <mitkovski.stefan.sm@gmail.com>
2025-01-11 22:37:22 +0100

    Recisi zavrsen delot vo komponentata LastTradedShres.vue

--- src/components/LastTradedShares.vue
+++ src/components/LastTradedShares.vue
@@ -1,49 +1,211 @@
 <template>
-<q-page-container class="col-md-10 offset-md-1">
-  <q-card flat bordered elevated class="q-pa-md">
-    <q-card-section>
-      <div class="text-h6 text-center">Last Traded Shares</div>
-      <div class="text-subtitle2 text-center">
-        A list of latest traded stock stored in the database
+  <q-page-container class="col-md-10 offset-md-1">
+    <q-card flat bordered elevated class="mobile-sticky-card">
+      <div class="column q-pa-sm">
+        <div class="row justify-center q-my-sm">
+          <div class="cursor-pointer text-center">
+            <b>Last Traded Shares</b> ({{ date }})
+          </div>
+        </div>
+
+        <div class="row items-center ">
+          <q-btn
+            class="col-auto"
+            flat
+            color="primary"
+            icon="fa-solid fa-list-check"
+          />
+
+          <div class="col">
+            <q-input
+              outlined
+              dense
+              v-model="searchReportString"
+              placeholder="Search by symbol..."
+              @update:model-value="searchReport"
+              hide-underline
+            >
+              <template v-slot:append>
+                <q-btn
+                  v-if="searchReportString.length === 0"
+                  flat
+                  round
+                  dense
+                  color="primary"
+                  icon="search"
+                  @click="searchReport"
+                />
+                <q-btn
+                  v-else
+                  flat
+                  round
+                  dense
+                  color="primary"
+                  icon="close"
+                  @click="clearSearch"
+                />
+              </template>
+            </q-input>
+          </div>
+        </div>
       </div>
-    </q-card-section>
-    <div>
-      <q-table
-        flat bordered
-        :rows="rows"
-        :columns="columns"
-        row-key="name"
-        :sort-method="customSort"
-        binary-state-sort
-      />
-    </div>
-  </q-card>
-</q-page-container>
+    </q-card>
+    <q-card flat bordered elevated class="q-pa-md">
+      <q-virtual-scroll
+        type="table"
+        style="max-height: 70vh"
+        :virtual-scroll-item-size="48"
+        :virtual-scroll-sticky-size-start="48"
+        :virtual-scroll-sticky-size-end="32"
+        :items="rows"
+      >
+        <template v-slot:before>
+          <thead class="thead-sticky sticky">
+            <tr>
+              <th v-for="col in columns" :key="col.name">
+                {{ col.name }}
+              </th>
+            </tr>
+          </thead>
+        </template>
+        <template v-slot="{ item }">
+          <tr>
+            <td class="text-center">{{ item.symbol }}</td>
+            <td class="text-center">
+              {{ isNaN(item.average_price) ? 0 : item.average_price }}
+            </td>
+            <td class="text-center">
+              {{ isNaN(item.change) ? 0 : item.change }}
+            </td>
+            <td class="text-center">
+              {{ isNaN(item.last_price) ? 0 : item.last_price }}
+            </td>
+            <td class="text-center">{{ isNaN(item.max) ? 0 : item.max }}</td>
+            <td class="text-center">{{ isNaN(item.min) ? 0 : item.min }}</td>
+            <td class="text-center">
+              {{ isNaN(item.purchase_price) ? 0 : item.purchase_price }}
+            </td>
+            <td class="text-center">
+              {{ isNaN(item.quantity) ? 0 : item.quantity }}
+            </td>
+            <td class="text-center">
+              {{ isNaN(item.sale_price) ? 0 : item.sale_price }}
+            </td>
+            <td class="text-center">
+              {{
+                isNaN(item.turnover_in_1000_den) ? 0 : item.turnover_in_1000_den
+              }}
+            </td>
+          </tr>
+        </template>
+      </q-virtual-scroll>
+    </q-card>
+  </q-page-container>
 </template>
-      
+
 <script>
-import httpUtils from 'src/assets/js/httpUtils';
-import { defineComponent } from 'vue'
-    
+import httpUtils from "src/assets/js/httpUtils";
+import { defineComponent } from "vue";
+
 export default defineComponent({
-    name: 'LastTradedShares',
-    props: {},
-    
-    data() {
+  name: "LastTradedShares",
+  props: {},
+
+  data() {
     return {
       rows: [],
-      columns: []
+      backupRows: [],
+      columns: [],
+      date: Date.now,
+      searchReportString: "",
     };
   },
   async created() {
     this.columns = [
-    { name: 'Symbol', align: 'left', label: 'Symbol', field: 'symbol', sortable: true,  format: val => `${val}` },
-    { name: 'Change', align: 'center', label: 'Change', field: 'change', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
-    { name: 'Last Price', align: 'center', label: 'Last Price', field: 'last_price', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
-    { name: 'Avg. Price', align: 'center', label: 'Avg. Price', field: 'average_price', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
-  ]
+      {
+        name: "Symbol",
+        field: "symbol",
+      },
+      {
+        name: "Avg. Price",
+        field: "average_price",
+      },
+      {
+        name: "Change",
+        field: "change",
+      },
+      {
+        name: "Last Price",
+        field: "last_price",
+      },
+      {
+        name: "Max",
+        field: "max",
+      },
+      {
+        name: "Min",
+        field: "min",
+      },
+      {
+        name: "Purchase Price",
+        field: "purchase_price",
+      },
+      {
+        name: "Quantity",
+        field: "quantity",
+      },
+      {
+        name: "Sale Price",
+        field: "sale_price",
+      },
+      {
+        name: "Turnover In 1000 den",
+        field: "turnover_in_1000_den",
+      },
+    ];
     this.rows = await httpUtils.getLatestTradedStocks();
-  }
-});    
+    this.backupRows = this.rows;
+    this.date = this.rows[0].date["$date"].substring(0, 10);
+  },
+
+  methods: {
+    searchReport() {
+      if (this.searchReportString.length === 0) {
+        this.rows = [...this.backupRows];
+      } else {
+        this.rows = this.backupRows.filter((item) =>
+          item.symbol
+            .toLowerCase()
+            .includes(this.searchReportString.toLowerCase())
+        );
+      }
+    },
+
+    clearSearch() {
+      this.searchReportString = "";
+      this.searchReport();
+    },
+  },
+});
 </script>
-      
+<style>
+.thead-sticky tr > * {
+  position: sticky;
+  opacity: 1;
+  z-index: 1;
+  background: white;
+}
+
+.thead-sticky tr:last-child > * {
+  top: 0;
+}
+.mobile-sticky-card {
+  max-width: 100vw;
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;  
+  left: 0;
+  /* z-index: 10; */
+  overflow-x: auto;
+}
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,14 +1,13 @@
 <template>
   <q-layout view="lHh Lpr lFf" class="row">
-
     <MainHeader />
 
-    <q-drawer v-if="$q.platform.is.mobile"
+    <q-drawer
+      v-if="$q.platform.is.mobile"
       v-model="leftDrawerOpen"
       show-if-above
       bordered
-      >
-
+    >
     </q-drawer>
 
     <LastTradedShares />
@@ -20,24 +19,21 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-import MainHeader from 'src/components/MainHeader.vue'
-import LastTradedShares from 'src/components/LastTradedShares.vue';
-
+import { defineComponent } from "vue";
+import MainHeader from "src/components/MainHeader.vue";
+import LastTradedShares from "src/components/LastTradedShares.vue";
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
   components: {
     MainHeader,
-    LastTradedShares
+    LastTradedShares,
   },
 
-  data(){
-    return {
-    }
+  data() {
+    return {};
   },
-  methods: {
-  }
-})
+  methods: {},
+});
 </script>
