3fe91beea6fecc16e19504ab4ed451badcf10607
Stefan Mitkovski <mitkovski.stefan.sm@gmail.com>
2025-01-19 13:28:01 +0100

    Prenesuvanje na elementi od edna strana vo druga

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,22 +12,6 @@
 
     <MainTradedShares />
 
-    <div class="row">
-  <div class="col-md-10 offset-md-1">
-    <div class="row justify-between q-gutter-md">
-      <div class="col-12 col-md-3">
-        <MostShares :rows="mostGainsReport" :title="'Most Gains'" />
-      </div>
-      <div class="col-12 col-md-3">
-        <MostShares :rows="mostLossesReport" :title="'Most Losses'" />
-      </div>
-      <div class="col-12 col-md-3">
-        <MostShares :rows="mostQunatityReport" :flag="true" :title="'Most Quantity'" />
-      </div>
-    </div>
-  </div>
-</div>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -38,8 +22,6 @@
 import { defineComponent } from "vue";
 import MainHeader from "src/components/MainHeader.vue";
 import MainTradedShares from "src/components/MainTradedShares.vue";
-import httpUtils from "src/assets/js/httpUtils";
-import MostShares from "src/components/MostShares.vue";
 
 export default defineComponent({
   name: "MainLayout",
@@ -47,62 +29,7 @@ export default defineComponent({
   components: {
     MainHeader,
     MainTradedShares,
-    MostShares,
   },
-
-  data() {
-    return {
-      weekReport: [],
-      mostGainsReport: [],
-      mostQunatityReport: [],
-      mostLossesReport: [],
-    };
-  },
-  async created() {
-    if (this.$q.sessionStorage.getItem("weekReport")) {
-      this.weekReport = JSON.parse(
-        this.$q.sessionStorage.getItem("weekReport")
-      );
-    } else {
-      this.$q.loading.show({
-        message: "Getting weekly data. Hang on...",
-      });
-
-      this.weekReport = await httpUtils.getWeeklyReport();
-
-      this.$q.sessionStorage.setItem(
-        "weekReport",
-        JSON.stringify(this.weekReport)
-      );
-    }
-
-    this.mostGainsReport = this.weekReport
-      .map((row) => ({
-        ...row,
-        change: parseFloat(row.change.toFixed(2)),
-      }))
-      .sort((a, b) => b.change - a.change)
-      .slice(0, 5);
-
-    this.mostQunatityReport = this.weekReport
-      .map((row) => ({
-        ...row,
-        quantity: parseFloat(row.quantity.toFixed(2)),
-      }))
-      .sort((a, b) => b.quantity - a.quantity)
-      .slice(0, 5);
-
-    this.mostLossesReport = this.weekReport
-      .map((row) => ({
-        ...row,
-        change: parseFloat(row.change.toFixed(2)),
-      }))
-      .sort((a, b) => a.change - b.change)
-      .slice(0, 5);
-
-    this.$q.loading.hide();
-  },
-
-  methods: {},
 });
 </script>
+
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,91 @@
 <template>
-  <q-page class="flex flex-center">
-    
+  <q-page>
+    <div class="row">
+      <div class="col-md-10 offset-md-1">
+        <div class="row justify-between q-gutter-md">
+          <div class="col-12 col-md-3">
+            <MostShares :rows="mostGainsReport" :title="'Most Gains'" />
+          </div>
+          <div class="col-12 col-md-3">
+            <MostShares :rows="mostLossesReport" :title="'Most Losses'" />
+          </div>
+          <div class="col-12 col-md-3">
+            <MostShares
+              :rows="mostQunatityReport"
+              :flag="true"
+              :title="'Most Quantity'"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
+import MostShares from "src/components/MostShares.vue";
+import httpUtils from "src/assets/js/httpUtils";
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: "IndexPage",
+
+  components: {
+    MostShares,
+  },
+
+  data() {
+    return {
+      weekReport: [],
+      mostGainsReport: [],
+      mostQunatityReport: [],
+      mostLossesReport: [],
+    };
+  },
+
+  async created() {
+    if (this.$q.sessionStorage.getItem("weekReport")) {
+      this.weekReport = JSON.parse(
+        this.$q.sessionStorage.getItem("weekReport")
+      );
+    } else {
+      this.$q.loading.show({
+        message: "Getting weekly data. Hang on...",
+      });
+
+      this.weekReport = await httpUtils.getWeeklyReport();
+
+      this.$q.sessionStorage.setItem(
+        "weekReport",
+        JSON.stringify(this.weekReport)
+      );
+    }
+
+    this.mostGainsReport = this.weekReport
+      .map((row) => ({
+        ...row,
+        change: parseFloat(row.change.toFixed(2)),
+      }))
+      .sort((a, b) => b.change - a.change)
+      .slice(0, 5);
+
+    this.mostQunatityReport = this.weekReport
+      .map((row) => ({
+        ...row,
+        quantity: parseFloat(row.quantity.toFixed(2)),
+      }))
+      .sort((a, b) => b.quantity - a.quantity)
+      .slice(0, 5);
+
+    this.mostLossesReport = this.weekReport
+      .map((row) => ({
+        ...row,
+        change: parseFloat(row.change.toFixed(2)),
+      }))
+      .sort((a, b) => a.change - b.change)
+      .slice(0, 5);
+
+    this.$q.loading.hide();
+  },
 });
 </script>
